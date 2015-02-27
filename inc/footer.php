39239e7f6777c5221e445c2c590c3d7269980d59
Edmundo Junior <hello@edmundojr.com>
2015-02-27 05:00:20 +0000

    Add better line breaking

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 		<footer role="contentinfo" class="footer container container--narrow cf">
-			<small>Made with love in Dublin. Follow me on <a href="https://twitter.com/edmundojr_">Twitter</a> and <a href="https://instagram.com/edmundojr_">Instagram</a>.</small>
+			<small>Made with love in Dublin. <span>Follow me on <a href="https://twitter.com/edmundojr_">Twitter</a> and <a href="https://instagram.com/edmundojr_">Instagram</a>.</span></small>
 		</footer>
 
 	</main>
