f063e0244d914cd13bd1ab7553cc6e28a05d8033
Edmundo Junior <hello@edmundojr.com>
2015-02-27 05:01:37 +0000

    Add blank work page

---
+++ work.php
@@ -0,0 +1,20 @@
+<?php include 'inc/functions.php'; get_header(); ?>
+
+		<div class="intro container container--narrow cf">
+			<div class="row">
+				<div class="col col-2-3">
+					<h1>Work it. Make it.<br> Do it. Makes us.</h1>
+					<p>Have a look at my portfolio, you'll find the latest of my personal projects and client work.</p>
+				</div>
+			</div>
+		</div>
+
+	</header>
+
+	<main class="main main--work color-bright" role="main">
+
+		<div class="container container--narrow cf">
+
+		</div>
+
+<?php get_footer(); ?>
