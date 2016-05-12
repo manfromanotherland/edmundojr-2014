d6be01497ee154ef752bca4511ffeab1d74782d1
Edmundo Santos <hello@edmundojr.com>
2016-05-12 04:37:43 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,62 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var shell = require('gulp-shell');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+// *************************************
+//  Gulpfile
+// *************************************
 
-// Task for building blog when something changed
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var shell        = require('gulp-shell');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync  = require('browser-sync').create();
+
+// -------------------------------------
+//  Options
+// -------------------------------------
+
+var options = {
+	sass: {
+		files	    : 'assets/scss/**/*.scss',
+		destination : '_site/assets/css/'
+	}
+};
+
+// -------------------------------------
+//  Task: build
+// -------------------------------------
+
 gulp.task('build', shell.task(['jekyll serve']));
 
-// Task for serving blog with Browsersync
+// -------------------------------------
+//  Task: serve with browser-sync
+// -------------------------------------
+
 gulp.task('serve', function () {
 	browserSync.init({server: {baseDir: '_site/'}});
-	// Reloads page when some of the already built files changed:
-	gulp.watch('assets/scss/**/*.scss', ['styles']);
+	gulp.watch(options.sass.files, ['styles']);
 	gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
-// Compile sass into CSS and run autoprefixer
+// -------------------------------------
+//  Task: styles
+// -------------------------------------
+
 gulp.task('styles', function() {
-	return gulp.src('assets/scss/**/*.scss')
+	return gulp.src(options.sass.files)
 		.pipe(sass({outputStyle: 'compressed'}))
 		.pipe(autoprefixer('last 2 versions', '> 2%'))
-		.pipe(gulp.dest('_site/assets/css/'))
+		.pipe(gulp.dest(options.sass.destination))
 		.pipe(browserSync.stream());
 });
 
-// Task for deploy the website
+// -------------------------------------
+//  Task: deploy
+// -------------------------------------
+
 gulp.task('deploy', shell.task(['jekyll build && rsync -vrzc --delete --exclude=wildside --exclude=bf --exclude=st _site/ edmundojr:www']));
+
+// -------------------------------------
+//  Task: default
+// -------------------------------------
 
 gulp.task('default', ['build', 'serve']);
